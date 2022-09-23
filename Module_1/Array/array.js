1df8920bff2ad48dac9c1addb51987c10063ae60
Khoa Nguyen <khoa.nguyen@lbhpartners.com>
2022-09-23 09:59:01 +0700

    array

---
+++ Module_1/Array/array.js
@@ -0,0 +1,32 @@
+// let jobs = ["Lau nhà"];
+// let jobs_2 = new Array("Lau nhà");
+// console.log(jobs.length);
+// console.log(jobs_2.length);
+
+// let numbers_1 = [5, 6]; // tạo 1 mảng, có 1 phần tử, giá trị là 5
+// let numbers_2 = new Array(5, 6); // tạp 1 mảng, có 5 phần tử, giá trị của mỗi phần tử là empty
+// console.log(numbers_1)
+// console.log(numbers_2)
+
+let jobs = ["Lau nhà", "Nấu cơm", "Quét nhà", "Đón con"];
+// console.log(jobs[0]);
+// console.log(jobs[1]);
+// console.log(jobs[2]);
+// console.log(jobs[3]);
+
+// for(let i = 0; i < jobs.length; i++){
+//     console.log(jobs[i])
+// }
+
+// let i = 0;
+// while(i < jobs.length){
+//     console.log(jobs[i]);
+//     i++;
+// }
+
+// let i = 0;
+// do{
+//     console.log(jobs[i]);
+//     i++;
+// }
+// while(i < jobs.length)
